1a4f0766d7bbe5e96adce428d7fecef09d7e2412
Chelsea Marfil <chelseamarfil@yahoo.com>
2018-10-24 15:09:49 -0700

    Initial Commit

---
+++ BlackjackProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BlackjackProject
+//
+//  Created by Chelsea Marfil on 10/24/18.
+//  Copyright © 2018 ChelseaMarfil. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
