5a3cbd27013efc665d42fced0b5cefef31956ec7
Chelsea Marfil <chelseamarfil@yahoo.com>
2018-11-28 14:48:37 -0800

    Updated getFaceValue() method in Card.cpp

--- BlackjackProject/Card.cpp
+++ BlackjackProject/Card.cpp
@@ -17,9 +17,35 @@ Card::Card(string mFace, string mSuit) {
     suit = mSuit;
 }
 
-//int Card::getFaceValue() {
-//    
-//}
+int Card::getFaceValue() {
+    if (face == "Ace") {
+        return 1;
+    } else if (face == "Two") {
+        return 2;
+    } else if (face == "Three") {
+        return 3;
+    } else if (face == "Four") {
+        return 4;
+    } else if (face == "Five") {
+        return 5;
+    } else if (face == "Six") {
+        return 6;
+    } else if (face == "Seven") {
+        return 7;
+    } else if (face == "Eight") {
+        return 8;
+    } else if (face == "Nine") {
+        return 9;
+    } else if (face == "Ten") {
+        return 10;
+    } else if (face == "Jack") {
+        return 10;
+    } else if (face == "King") {
+        return 10;
+    } else if (face == "Queen") {
+        return 10;
+    }
+}
 
 string Card::print() const {
     return (face + " of " + suit);
