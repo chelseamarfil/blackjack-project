da8aa22aa6875dc4a8bf1a58a4daf0940ec2da49
Rachel Pai <racheljpai@gmail.com>
2018-12-05 14:34:34 -0800

    Added a comment.

--- BlackjackProject/main.cpp
+++ BlackjackProject/main.cpp
@@ -8,7 +8,8 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[]) 
+{
     // a. Ask the user to enter the account number.
     // b. Ask the user to enter the amount of money he/she wants to bet.
     // c. The program will randomly select and show the user two cards.
