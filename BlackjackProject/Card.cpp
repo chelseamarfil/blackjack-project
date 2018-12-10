d7161c33a743eb8fbee95cf531a8a0f45548cd1d
Rachel Pai <44449816+pairachel@users.noreply.github.com>
2018-12-10 15:13:00 -0800

    Revert Card.cpp

--- BlackjackProject/Card.cpp
+++ BlackjackProject/Card.cpp
@@ -3,8 +3,8 @@
 //  BlackjackProject
 //
 //  Created by Chelsea Marfil on 11/5/18.
-//  Copyright © 2018 ChelseaMarfil. All rights reserved.
-//
+//  Copyright Â© 2018 ChelseaMarfil. All rights reserved.
+// 
 
 #include "Card.h"
 
